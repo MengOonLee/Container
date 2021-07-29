e3a694acb4479850f3d50762faf83ec97b6664de
MengOonLee <darklemon2000@gmail.com>
2021-07-29 18:05:35 +0800

    Airflow with GitSync

--- Airflow/Helm/install.sh
+++ Airflow/Helm/install.sh
@@ -2,13 +2,11 @@
 
 microk8s helm3 repo add apache-airflow https://airflow.apache.org
 microk8s helm3 repo update
+microk8s kubectl create namespace airflow
+microk8s kubectl create secret generic airflow-ssh-secret --from-file=gitSshKey=/home/meng/.ssh/airflow -n airflow
 
 microk8s helm3 upgrade --cleanup-on-fail \
 --install airflow apache-airflow/airflow \
 --namespace airflow \
---create-namespace \
 --values values.yaml \
---set images.airflow.repository=darklemon/airflow_dags \
---set images.airflow.tag=latest \
---set images.airflow.pullPolicy=Always \
 --debug
