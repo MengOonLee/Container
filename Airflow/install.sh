5cc0e4c4cbdd358b7ceef8b68508f8c855cc8db9
MengOonLee <darklemon2000@gmail.com>
2021-07-30 10:41:59 +0800

    Change to Notebook

--- Airflow/install.sh
+++ Airflow/install.sh
@@ -3,7 +3,6 @@
 microk8s helm3 repo add apache-airflow https://airflow.apache.org
 microk8s helm3 repo update
 microk8s kubectl create namespace airflow
-microk8s kubectl create secret generic airflow-ssh-secret --from-file=gitSshKey=/home/meng/.ssh/airflow -n airflow
 
 microk8s helm3 upgrade --cleanup-on-fail \
 --install airflow apache-airflow/airflow \
