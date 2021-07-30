56b6386e917c914c335a6729373931b5e3f65bf6
MengOonLee <darklemon2000@gmail.com>
2021-07-30 11:18:18 +0800

    copy dags in Dockerfile

--- Airflow/install.sh
+++ Airflow/install.sh
@@ -7,5 +7,7 @@ microk8s kubectl create namespace airflow
 microk8s helm3 upgrade --cleanup-on-fail \
 --install airflow apache-airflow/airflow \
 --namespace airflow \
---values values.yaml \
+--set images.airflow.repository=darklemon/airflow \
+--set images.airflow.tag=latest \
+--set images.airflow.pullPolicy=Always \
 --debug
