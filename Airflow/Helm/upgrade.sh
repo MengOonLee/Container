03cca0f4f47f8e7abd29d679420124a8f3d20cf3
MengOonLee <darklemon2000@gmail.com>
2021-12-22 19:39:14 +0800

    Airflow: webserver secret

--- Airflow/Helm/upgrade.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-microk8s helm3 upgrade airflow apache-airflow/airflow \
---namespace airflow \
---set images.airflow.repository=darklemon/test-airflow-dag \
---set images.airflow.tag=0.0.1 \
---set images.airflow.pullPolicy=Always
