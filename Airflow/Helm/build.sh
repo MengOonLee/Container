18ca59d529091a21b1e30457121a9e8f99453b36
MengOonLee <darklemon2000@gmail.com>
2021-12-28 19:03:42 +0800

    Airflow: NodePort

--- Airflow/Helm/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker build . --no-cache \
--f Dockerfile \
--t "darklemon/test-airflow-dag:0.0.1"
-
-docker push darklemon/test-airflow-dag:0.0.1
--- Airflow/Helm/install.sh
+++ Airflow/Helm/install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 microk8s helm3 repo add apache-airflow https://airflow.apache.org
 microk8s helm3 repo update
 microk8s kubectl create -f namespace.yaml
@@ -7,6 +6,4 @@ microk8s kubectl create -f secret.yaml
 
 microk8s helm3 install airflow apache-airflow/airflow \
 --namespace airflow \
---set executor=CeleryKubernetesExecutor \
---set webserverSecretKeySecretName=airflow-secret \
---set webserverSecretKey=webserver-secret-key
+--values override.yaml
