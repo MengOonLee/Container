2e766e10947ebb8c19779e2fb4507b05aaa30b8f
MengOonLee <darklemon2000@gmail.com>
2022-08-05 18:21:32 +0800

    Airflow: test

--- Airflow/Helm/install.sh
+++ Airflow/Helm/install.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
 
-# export REPOSITORY="darklemon"
-# export IMAGE="airflow"
-# export TAG="latest"
-
-# docker build . --no-cache \
-# -f Dockerfile \
-# -t $REPOSITORY/$IMAGE:$TAG
-
-# docker push $REPOSITORY/$IMAGE:$TAG
-
 microk8s helm3 repo add apache-airflow https://airflow.apache.org
 microk8s helm3 repo update
-microk8s helm3 show values apache-airflow/airflow > values.yaml
 
-microk8s kubectl create -f namespace.yaml
+microk8s kubectl create namespace airflow
 microk8s kubectl create -f pv_dags.yaml
 microk8s kubectl create -f pvc_dags.yaml
-microk8s kubectl create -f secret.yaml
 
 microk8s helm3 upgrade \
 --install airflow apache-airflow/airflow \
