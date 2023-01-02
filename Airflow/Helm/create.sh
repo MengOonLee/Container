8cef11417b3f749cec2e2417dd1f36e04c7e965e
MengOonLee <darklemon2000@gmail.com>
2023-01-02 10:22:06 +0800

    remove Airflow

--- Airflow/Helm/create.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-microk8s helm3 repo add apache-airflow https://airflow.apache.org
-microk8s helm3 repo update
-
-microk8s kubectl create namespace airflow
-microk8s kubectl create -f pv_dags.yaml
-microk8s kubectl create -f pvc_dags.yaml
